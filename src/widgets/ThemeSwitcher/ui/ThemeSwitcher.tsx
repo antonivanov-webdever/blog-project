d49b436ea58537d9060797e857c2067ed2f59b68
anton.ivanov <anton.ivanov@netwrix.com>
2023-02-11 15:12:16 +0400

    added stylelint and eslint i18next plugin

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import { Theme, useTheme } from 'shared/contexts';
 import IconDark from 'shared/assets/theme-dark.svg';
 import IconLight from 'shared/assets/theme-light.svg';
 import { AppButton, AppButtonTheme } from 'shared/ui/AppButton/AppButton';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -13,7 +12,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
     return (
         <AppButton
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
             theme={AppButtonTheme.CLEAR}
         >
